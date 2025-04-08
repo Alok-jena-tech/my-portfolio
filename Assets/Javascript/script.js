464135faee3f01528de44b29e35ea99188898433
Alok-jena-tech <alokjena3896@gmail.com>
2025-04-08 09:52:28 +0530

    this is about my portfolio

---
+++ Assets/Javascript/script.js
@@ -0,0 +1,9 @@
+function About_me(){
+    document.getElementById('About_me').scrollIntoView()
+}
+function Expert_sec(expert_se){
+    document.getElementById(expert_se).scrollIntoView({behavior:'smooth'})
+}
+function Contact_sec(contact){
+    document.getElementById(contact).scrollIntoView({bahavior:'smooth'})
+}
